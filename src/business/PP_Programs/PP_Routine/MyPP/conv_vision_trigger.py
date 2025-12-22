8c0137f2e356a7ae4a736ffe24315818baad4a77
ann <799045150@qq.com>
2025-12-22 13:55:47 +0800

    python版的电机控制

--- src/business/PP_Programs/PP_Routine/MyPP/conv_vision_trigger.py
+++ src/business/PP_Programs/PP_Routine/MyPP/conv_vision_trigger.py
@@ -21,33 +21,38 @@ class ConvVisionTrigger(ParallelProgram):
 
     def pp_conv_vision_trigger(self):
         cam_intr_params = '919.49866 918.7968 648.15015 375.07938 1280 720 0.001'
-        cam_extr_params = '0 0 0 0 0 0'
+        cam_extr_params = '0 0 0 0 0 0'  
         obj_name = 'Conveyor-SerialPort-1/Vision'
         recv_text = ''
-        while True:
-            if socket_open(1, '192.168.2.112', 30000) :
-                socket_connect = socket_connected(1)
-                if  socket_connect:
-                    print('Connection success')
-                while socket_connect:
-                    # if get_io(GPIOEnum.SYSTEM,GPIOInPortEnum.GPIO_IN_0 ) == 1 :
-                    if not socket_send(1, 'Trigger') :
-                        print('Send fail')
-                        break
-                    recv_text = socket_recv(1)
-                    if recv_text != '' :
-                        obj_param = split_string(recv_text, ';')
-                        obj_value = get_list(obj_param,1)
-                        update_object_pool(obj_name,8 ,obj_value, cam_intr_params, cam_extr_params, 'flange')
-                    wait_ms(100)
-            else :
-                self.func_enter_silent_mode()
+        if socket_open(1, '192.168.2.50', 30000) :
+            while True:
+                if not socket_connected(1):
+                    print('Connection fail')
+                    break
+                # if get_io(GPIOEnum.SYSTEM,GPIOInPortEnum.GPIO_IN_0 ) == 1 :
+                if not socket_send(1, 'Trigger') :
+                    print('Send fail')
+                    break
+                recv_text = socket_recv(1)
+                if recv_text != '' :
+                    obj_param = split_string(recv_text, ';')
+                    obj_value = obj_param[1]
+                    update_object_pool(
+                        obj_name,
+                        8 ,
+                        obj_value,
+                        cam_intr_params,
+                        cam_extr_params,
+                        'flange',
+                    )
+        else :
+            self.func_enter_silent_mode()
         
 
     def func_enter_silent_mode(self):
         """进入静默模式"""
         print('Loss connection')
-        while not socket_open(1, '192.168.2.112', 30000):
+        while not socket_open(1, '192.168.2.50', 30000):
             if get_system_state(SystemStateEnum.IS_FAULT):
                 clear_fault()
             wait_ms(30)
--- src/business/PP_Programs/PP_Routine/MyPP/pp_motor.py
+++ src/business/PP_Programs/PP_Routine/MyPP/pp_motor.py
@@ -33,7 +33,6 @@ class USBMotorTxClient(ParallelProgram):
         super().__init__(setting=setting)
 
         self.FRAME_HEADER = 0x3E
-        self.SINGLE_TURN_CMD_V1 = 0xA5
         self.SINGLE_TURN_CMD_V2 = 0xA6
         self.READ_SINGLE_ANGLE_CMD = 0x94
         self.SET_ZERO_POINT_CMD = 0x19
@@ -55,7 +54,8 @@ class USBMotorTxClient(ParallelProgram):
             # print([0x3E, 0xA5, 0x01, 0x04, 0xE8, 0x00, 0x1E, 0x46, 0x00, 0x64])
             # resp = serial_port_recv(1, 16)
             # print(resp)
-            cmd_list = self.func_send_angle(300.0, 0x00)
+            # cmd_list = self.func_send_angle_v2(30.0, 0x00, 50)
+            cmd_list = self.func_move_to_angle_v2(20.0, 75, motor_id=1)
             print(cmd_list)
             serial_port_send(1, cmd_list)
             resp = serial_port_recv(1, 16)
@@ -93,7 +93,7 @@ class USBMotorTxClient(ParallelProgram):
 
         # 1. 构造帧[0]~[4]
         frame_list = []
-        angleIncrement = angle * 8 * 100.0
+        angleIncrement = math.floor(angle * 8 * 100.0)
         frame_header_num = to_number("0x3E", NumberFormatEnum.DEC)
         single_turn_cmd_v1_num = to_number("0xA5", NumberFormatEnum.DEC)
         default_motor_id_num = to_number("0x01", NumberFormatEnum.DEC)
@@ -125,5 +125,108 @@ class USBMotorTxClient(ParallelProgram):
         data_sum_low = (direction_num + low_angle_num + high_angle_num + to_number(angleControl_str, NumberFormatEnum.DEC)) % 256
         append_list(frame_list, data_sum_low)
         return frame_list
+
+    def func_send_angle_v2(self, angle: float, direction: int, speed_percentage: int, motor_id: int = None):
+        if not (0.0 <= angle <= 359.99):
+            print("角度需在0~359.99范围内")
+
+        frame_list = []
+        frame_header_num = to_number("0x3E", NumberFormatEnum.DEC)
+        single_turn_cmd_v2_num = to_number("0xA6", NumberFormatEnum.DEC)
+        mid = motor_id
+        if mid == None:
+            mid = 1
+        if mid < 1 or mid > 32:
+            print("电机ID需在1~32范围内")
+            mid = 1
+        mid_mod = mid % 256
+        motor_id_str = concat_string("0x", to_string(mid_mod, NumberFormatEnum.HEX))
+        motor_id_num = to_number(motor_id_str, NumberFormatEnum.DEC)
+        data_len_num = to_number("0x08", NumberFormatEnum.DEC)
+        append_list(frame_list, frame_header_num)
+        append_list(frame_list, single_turn_cmd_v2_num)
+        append_list(frame_list, motor_id_num)
+        append_list(frame_list, data_len_num)
+        header_sum_low = (frame_header_num + single_turn_cmd_v2_num + motor_id_num + data_len_num) % 256
+        append_list(frame_list, header_sum_low)
+
+        direction_str = concat_string("0x", to_string(direction, NumberFormatEnum.HEX))
+        direction_num = to_number(direction_str, NumberFormatEnum.DEC)
+
+        angle_increment = math.floor(angle * 8 * 100.0)
+        angle_str = concat_string("0x", to_string(angle_increment, NumberFormatEnum.HEX))
+        angle_low_str = concat_string("0x", to_string(to_number(angle_str, NumberFormatEnum.HEX) % 256, NumberFormatEnum.HEX))
+        angle_mid_str = concat_string("0x", to_string(math.floor(to_number(angle_str, NumberFormatEnum.HEX) / 256) % 256, NumberFormatEnum.HEX))
+        angle_high_str = concat_string("0x", to_string(math.floor(to_number(angle_str, NumberFormatEnum.HEX) / 65536) % 256, NumberFormatEnum.HEX))
+        angle_low_num = to_number(angle_low_str, NumberFormatEnum.DEC)
+        angle_mid_num = to_number(angle_mid_str, NumberFormatEnum.DEC)
+        angle_high_num = to_number(angle_high_str, NumberFormatEnum.DEC)
+
+        max_speed_val = math.floor(600000 * (speed_percentage / 100.0))
+        speed_str = concat_string("0x", to_string(max_speed_val, NumberFormatEnum.HEX))
+        speed_b0_str = concat_string("0x", to_string(to_number(speed_str, NumberFormatEnum.HEX) % 256, NumberFormatEnum.HEX))
+        speed_b1_str = concat_string("0x", to_string(math.floor(to_number(speed_str, NumberFormatEnum.HEX) / 256) % 256, NumberFormatEnum.HEX))
+        speed_b2_str = concat_string("0x", to_string(math.floor(to_number(speed_str, NumberFormatEnum.HEX) / 65536) % 256, NumberFormatEnum.HEX))
+        speed_b3_str = concat_string("0x", to_string(math.floor(to_number(speed_str, NumberFormatEnum.HEX) / 16777216) % 256, NumberFormatEnum.HEX))
+        speed_b0_num = to_number(speed_b0_str, NumberFormatEnum.DEC)
+        speed_b1_num = to_number(speed_b1_str, NumberFormatEnum.DEC)
+        speed_b2_num = to_number(speed_b2_str, NumberFormatEnum.DEC)
+        speed_b3_num = to_number(speed_b3_str, NumberFormatEnum.DEC)
+
+        append_list(frame_list, direction_num)
+        append_list(frame_list, angle_low_num)
+        append_list(frame_list, angle_mid_num)
+        append_list(frame_list, angle_high_num)
+        append_list(frame_list, speed_b0_num)
+        append_list(frame_list, speed_b1_num)
+        append_list(frame_list, speed_b2_num)
+        append_list(frame_list, speed_b3_num)
+        data_sum_low = (direction_num + angle_low_num + angle_mid_num + angle_high_num + speed_b0_num + speed_b1_num + speed_b2_num + speed_b3_num) % 256
+        append_list(frame_list, data_sum_low)
+        return frame_list
+
+    def func_read_single_angle(self, motor_id: int = None):
+        frame_list = []
+        frame_header_num = to_number("0x3E", NumberFormatEnum.DEC)
+        read_single_angle_cmd_num = to_number("0x94", NumberFormatEnum.DEC)
+        mid = motor_id
+        if mid == None:
+            mid = 1
+        if mid < 1 or mid > 32:
+            print("电机ID需在1~32范围内")
+            mid = 1
+        mid_mod = mid % 256
+        motor_id_str = concat_string("0x", to_string(mid_mod, NumberFormatEnum.HEX))
+        motor_id_num = to_number(motor_id_str, NumberFormatEnum.DEC)
+        data_len_num = to_number("0x00", NumberFormatEnum.DEC)
+        append_list(frame_list, frame_header_num)
+        append_list(frame_list, read_single_angle_cmd_num)
+        append_list(frame_list, motor_id_num)
+        append_list(frame_list, data_len_num)
+        header_sum_low = (frame_header_num + read_single_angle_cmd_num + motor_id_num + data_len_num) % 256
+        append_list(frame_list, header_sum_low)
+        serial_port_send(1, frame_list)
+        resp = serial_port_recv(1, 10)
+        if resp:
+            cmd_sum_low = (resp[0] + resp[1] + resp[2] + resp[3]) % 256
+            data_sum_low = (resp[5] + resp[6] + resp[7] + resp[8]) % 256
+            raw_value = resp[5] + resp[6] * 256 + resp[7] * 65536 + resp[8] * 16777216
+            angle_deg = (raw_value * 0.01) / 8
+            return angle_deg
+        return 0.0
+
+    def func_move_to_angle_v2(self, target_angle: float, speed_percentage: int, motor_id: int = None):
+        current_angle = self.func_read_single_angle(motor_id)
+        cw = (target_angle - current_angle + 360.0) % 360.0
+        ccw = (current_angle - target_angle + 360.0) % 360.0
+        direction = 0
+        delta = cw
+        if cw <= ccw:
+            direction = 0x00
+            delta = cw
+        else:
+            direction = 0x01
+            delta = ccw
+        return self.func_send_angle_v2(delta, direction, speed_percentage, motor_id)
 
     
